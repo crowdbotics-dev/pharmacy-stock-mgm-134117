f4bba0bd1a5c2df7ad91a7d5df6c1f8489c7052c
Crowdbotics <engineering@crowdbotics.com>
2024-02-28 23:38:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pharmacy-stock-mgm-134117",
+  url: "https://pharmacy-stock-mgm-134117.botics.co",
+  api: "https://pharmacy-stock-mgm-134117.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
